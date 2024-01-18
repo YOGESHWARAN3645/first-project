8b7639bca3adb2a31227833ef65d46288b7430e9
Yogeshwaran D <iiot@lmw.co.in>
2024-01-18 16:14:10 +0530

    Created db, html file templates

---
+++ members/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('home/', views.home),
+    path('login/', views.login),
+    path('members/', views.members),
+    path('members/details/<int:id>', views.details),
+    path('test/', views.testing),
+    
+]
---
+++ members/views.py
@@ -0,0 +1,42 @@
+
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+from django.views.decorators.http import require_http_methods
+from django.shortcuts import render
+
+def members(request):
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+  
+def details(request, id):
+  mymember = Member.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+    
+def home(request):
+  template= loader.get_template('home.html')
+  return HttpResponse(template.render())
+
+def login(request):
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('login.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+
+def testing(request):
+  mydata = Member.objects.values_list('firstname')
+  template = loader.get_template('query.html')
+  context = {
+    'mymembers': mydata,
+  }
+  return HttpResponse(template.render(context, request))
