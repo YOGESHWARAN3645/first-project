85e18d73a27047e1593749a09a1ce9cf8ed0f3b5
YOGESHWARAN D <yogeshdhandapani45@gmail.com>
2024-02-06 09:28:17 +0530

    1.0

--- members/urls.py
+++ members/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.login),
+    path('logindetailspage/',views.logindetails),
     path('home/', views.home),
-    path('login/', views.login),
     path('members/', views.members),
     path('members/details/<int:id>', views.details),
     path('test/', views.testing),
-    path('login/logindetailspage/',views.logindetails)
     
 ]
--- members/views.py
+++ members/views.py
@@ -1,45 +1,33 @@
 
-from django.http import HttpResponse
-from django.template import loader
 from .models import Member
-from django.views.decorators.http import require_http_methods
 from django.shortcuts import render
 
 def members(request):
   mymembers = Member.objects.all().values()
-  template = loader.get_template('all_members.html')
-  context = {
-    'mymembers': mymembers,
-  }
-  return HttpResponse(template.render(context, request))
-  
+  context = {'mymembers': mymembers,}
+  return render(request,'all_members.html',context)
+
 def details(request, id):
   mymember = Member.objects.get(id=id)
-  template = loader.get_template('details.html')
   context = {
     'mymember': mymember,
   }
-  return HttpResponse(template.render(context, request))
-    
+  return render(request,'details.html',context)
+
 def home(request):
-  template= loader.get_template('home.html')
-  return HttpResponse(template.render())
+  return render(request,'home.html')
 
 def login(request):
   mymembers = Member.objects.all().values()
-  template = loader.get_template('login.html')
-  context = {
-    'mymembers': mymembers,
-  }
-  return HttpResponse(template.render(context, request))
+  context = {'mymembers': mymembers,}
+  return render(request,'login.html',context)
 
 def testing(request):
   mydata = Member.objects.values_list('firstname')
-  template = loader.get_template('query.html')
   context = {
     'mymembers': mydata,
   }
-  return HttpResponse(template.render(context, request))
+  return render(request,'query.html',context)
 
 def logindetails(request):
     username = request.GET["name"]
